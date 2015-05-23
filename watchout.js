f819c6dbfb851607393c4f004e6895bb46b2552c
Michael Symmes <msymmes@gmail.com>
2015-05-23 13:43:36 -0400

    Implemented size and color changing enemies

--- watchout.js
+++ watchout.js
@@ -2,8 +2,12 @@
 
 var enemies = [];
 for (var i = 0; i <= 25; i++) {
-  enemies.push(0);
+  enemies.push(Math.floor(Math.random() * (20 - 5) + 5));
 }
+
+var randy = function(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
 var player = [0];
 
@@ -14,22 +18,13 @@ var svg = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height);
 // .append("g");
-  // .attr("transform", "translate(32," + (height / 2) + ")");
-
-////////////////////////////////////////////////////////
 
 var drag = d3.behavior.drag()
   .origin(function(d) {
     return d;
   })
-  .on("dragstart", dragstarted)
-  .on("drag", dragged)
-  .on("dragend", dragended);
 
-function dragstarted(d) {
-  d3.event.sourceEvent.stopPropagation();
-  d3.select(this).classed("dragging", true);
-}
+.on("drag", dragged);
 
 function dragged(d) {
   var grabbedPlayer = d3.select(this);
@@ -38,40 +33,22 @@ function dragged(d) {
       return +grabbedPlayer.attr("x") + d3.event.dx;
     })
     .attr("y", function(d) {
-     return +grabbedPlayer.attr("y") + d3.event.dy;
-   });
+      return +grabbedPlayer.attr("y") + d3.event.dy;
+    });
 }
-
-function dragended(d) {
-  d3.select(this).classed("dragging", false);
-}
-// function dragmove(d) {
-//   console.log(this.x, d3.event.dx);
-//   d3.select(this)
-//       .attr("x",  function(d, i) {
-//         console.log("x:", i);
-//         return d + d3.event.dx;
-//       })
-//       .attr("y",  function(d, i ) {
-//         console.log("y: ", i);
-//         return d + d3.event.dy;});
-// }
-
-
-//////////////////////////////////////////////////////
 
 var update = function(enemy, hero) {
+
   var enemies = svg.selectAll("circle")
     .data(enemy);
 
   enemies.attr("class", "enemies");
 
   enemies.enter().append("circle")
-    .attr("r", 10)
+    .attr("r", function(d) { return d;})
     .attr("cx", width / 2)
     .attr("cy", height / 2)
     .classed("enemies", true);
-
 
   var player = svg.selectAll("rect")
     .data(hero)
@@ -84,22 +61,23 @@ var update = function(enemy, hero) {
     .attr("y", height / 2)
     .classed("player", true);
 
-
-
-  enemies.attr("cx", function() {
-    return Math.floor(Math.random() * (width - 0));
-  });
-  enemies.attr("cy", function() {
-    return Math.floor(Math.random() * (height - 0));
-  });
-
-
+  enemies
+    .transition()
+    .duration(1000)
+    .style("fill", function() { return "rgb("+randy(0, 255)+", "+randy(0, 255)+", "+randy(0, 255)+")" })
+    .attr("r", function() { return Math.floor(Math.random() * (20 - 5) + 5); })
+    .attr("cx", function() {
+      return Math.floor(Math.random() * (width - 0));
+    })
+    .attr("cy", function() {
+      return Math.floor(Math.random() * (height - 0));
+    });
 };
+
 update(enemies, player);
 
 setInterval(function() {
-      update(enemies, player);
-    }, 1000 );
-
+  update(enemies, player);
+}, 1000);
 
 
