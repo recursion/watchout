8bf11d9c853cf20f87d69271f567396985d850ef
Michael Symmes <msymmes@gmail.com>
2015-05-23 14:00:04 -0400

    Cleaned up code

--- watchout.js
+++ watchout.js
@@ -1,42 +1,32 @@
 // start slingin' some d3 here.
 
+var player = [0];
+var highScore = 0;
+var currentScore = 0;
+var collisions = 0;
+
+// Create and initialize enemies
 var enemies = [];
 for (var i = 0; i <= 25; i++) {
   enemies.push(Math.floor(Math.random() * (20 - 5) + 5));
 }
 
-var randy = function(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-};
-
-var player = [0];
-
 var width = 960,
-  height = 500;
+  height = 1000;
 
+// Create our SVG game board
 var svg = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height);
-// .append("g");
 
+// drag behavior
 var drag = d3.behavior.drag()
   .origin(function(d) {
     return d;
   })
+  .on("drag", dragged);
 
-.on("drag", dragged);
-
-function dragged(d) {
-  var grabbedPlayer = d3.select(this);
-  grabbedPlayer
-    .attr("x", function(d) {
-      return +grabbedPlayer.attr("x") + d3.event.dx;
-    })
-    .attr("y", function(d) {
-      return +grabbedPlayer.attr("y") + d3.event.dy;
-    });
-}
-
+// Main update method - all the action is here
 var update = function(enemy, hero) {
 
   var enemies = svg.selectAll("circle")
@@ -64,6 +54,11 @@ var update = function(enemy, hero) {
   enemies
     .transition()
     .duration(1000)
+    .tween("collision", function(d) {
+      var grabbedPlayer = d3.select("rect");
+      //console.log(grabbedPlayer.attr("x"), grabbedPlayer.attr("x"));
+      //console.log(this);
+    })
     .style("fill", function() { return "rgb("+randy(0, 255)+", "+randy(0, 255)+", "+randy(0, 255)+")" })
     .attr("r", function() { return Math.floor(Math.random() * (20 - 5) + 5); })
     .attr("cx", function() {
@@ -80,4 +75,23 @@ setInterval(function() {
   update(enemies, player);
 }, 1000);
 
+//////////////////////////////////////////////
+//      HELPER FUNCTIONS
+////////////////////////////////////////////
 
+// Create a random number between min and max
+function randy(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+// Drag handler
+function dragged(d) {
+  var grabbedPlayer = d3.select(this);
+  grabbedPlayer
+    .attr("x", function(d) {
+      return +grabbedPlayer.attr("x") + d3.event.dx;
+    })
+    .attr("y", function(d) {
+      return +grabbedPlayer.attr("y") + d3.event.dy;
+    });
+};
